e1991041df3e472fc04f0a9f386193c8f3955913
Andrea Perin <xinandre@gmail.com>
2023-06-05 15:05:50 +0300

    Better naming conventions

--- random_loop.py
+++ random_loop.py
@@ -5,7 +5,7 @@ Much faster than the GP method, probably best to use this one
 from jax import numpy as jnp, random as jrand
 
 
-def get_loop(key, N, H=5, rmin=-2.5, rmax=-.5):
+def get_polar_loop(key, N, H=5, rmin=-2.5, rmax=-.5):
     # sample radii and phases
     rp = jrand.uniform(key, (2*H, ))
     rho = (rp[:H] * jnp.logspace(rmin, rmax, H))[:, None]
@@ -24,6 +24,6 @@ if __name__ == "__main__":
     import matplotlib.pyplot as plt
     SEED = 0
     rng = jrand.PRNGKey(SEED)
-    xs, ys = get_loop(rng, 1000, H=12, rmin=-2, rmax=-1)
+    xs, ys = get_polar_loop(rng, 1000, H=12, rmin=-2, rmax=-1)
     plt.plot(xs, ys)
     plt.show()
